aa869208ef5ffb00943bedfaef817671e07493ba
Peardox <simon@peardox.com>
2021-09-25 15:23:46 +0100

    Improve UI

--- code/gamestatemain.pas
+++ code/gamestatemain.pas
@@ -5,12 +5,14 @@
   is covered by the LGPL license variant, see the COPYING.txt file.) }
 unit GameStateMain;
 
+// {$define userenderstack}
+
 interface
 
 uses Classes, Math,
   CastleVectors, CastleUIState, CastleComponentSerialize,
   CastleUIControls, CastleControls, CastleKeysMouse, CastleViewport,
-  CastleCameras, CastleApplicationProperties, CastleSceneCore,
+  CastleCameras, CastleApplicationProperties, CastleSceneCore, CastleGLVersion,
   CastleWindow, CastleScene, X3DNodes, X3DFields, X3DLoad, CastleTimeUtils;
 
 type
@@ -39,7 +41,7 @@ type
     LabelKeys:       TCastleLabel;
     LabelAction:     TCastleLabel;
     LabelFirstFrame: TCastleLabel;
-	LabelBootStrap:  TCastleLabel;
+    LabelBootStrap:  TCastleLabel;
     Viewport:        TCastleViewport;
     Scene:           TCastleScene;
     SecsPerRot:      Single;        // Complete one rotation in this many seconds
@@ -49,13 +51,17 @@ type
     RotateSphere:    Boolean;       // Is the Sphere Rotating
     CameraAtOrigin:  Boolean;       // Is Camera at (0, 0, 0)
     Models:          TModelArray;   // Models used to create the Sphere
-	LoadTimer:		 Int64;			// Loading timer
+    LoadTimer:       Int64;	    // Loading timer
+    BootStrapTimer:  Int64;         // BootStrap timer
     DoneBootstrap:   Boolean;       // To prevent mutiple bootstraps
+    AbortEverything: Boolean;       // Immediately stop everything
     function    BuildSphere(const SomeModels: TModelArray; const ObjCount: Integer = 1): TX3DRootNode;
     procedure   LoadScene(const SomeModels: TModelArray; const ObjectsOnSphere: Integer = 1);
+    procedure   LoadSceneOnUpdate(Sender: TObject);
     procedure   SetCameraPosition;
-    procedure   SaveTheta;
-	procedure   BootStrap;
+    procedure   SaveCurrentTheta;
+    function    RebaseSavedTheta(const ADelta: Single): Single;
+    procedure   BootStrap;
   public
     constructor Create(AOwner: TComponent); override;
     procedure   Start; override;
@@ -70,6 +76,8 @@ const
   ScaleMultiplier = 0.5;
   MinSphereObjs   = 64;
   ViewDistance    = 3;
+  BootCount       = 5;
+  BootStrapLabel  = 'Press the Spacebar to begin' + LineEnding;
   ModelFiles: Array [0..20] of String = ('Alien.glb','Alien_Tall.glb','Bat.glb',
 			  'Bee.glb','Cactus.glb','Chicken.glb','Crab.glb','Cthulhu.glb',
 			  'Cyclops.glb','Deer.glb','Demon.glb','Ghost.glb','GreenDemon.glb',
@@ -140,11 +148,13 @@ begin
   DesignUrl := 'castle-data:/gamestatemain.castle-user-interface';
   SavedTheta := 0;
   SecsPerRot := 15;
-  SphereObjs := 128;
+  SphereObjs := 64;
   RotateSphere := True;
   CameraAtOrigin := False;
   LoadTimer := 0;
+  BootStrapTimer := CastleGetTickCount64;
   DoneBootstrap := False;
+  AbortEverything := False;
 
   SetLength(Models, High(ModelFiles) + 1);
   for i := Low(Models) to High(Models) do
@@ -169,11 +179,12 @@ begin
 
   LabelAction.Caption := '';
   LabelFirstFrame.Caption := '';
+  LabelBootStrap.Caption := '';
   
   LabelKeys.Caption := 'Control Keys' + LineEnding +
     'D = Double Number of Objects' + LineEnding +
     'H = Halve Number of Objects' + LineEnding +
-    'X = Toggle Control Display' + LineEnding +
+    'X = Toggle Controls + Info Display' + LineEnding +
     'R = Toggle Sphere Rotation' + LineEnding +
     'C = Position Camera Inside/Outside Sphere' + LineEnding +
     'G = Grab Screen (grab.png)' + LineEnding +
@@ -190,31 +201,53 @@ procedure TStateMain.BootStrap;
 begin
   if not DoneBootstrap then
     begin
+      BootStrapTimer := 0;
       LoadScene(Models, SphereObjs);
       DoneBootstrap := True;
       LabelKeys.Exists := True;
-	  LabelBootStrap.Exists := False;
-	end;
+      LabelBootStrap.Exists := False;
+    end;
 end;
 
 procedure TStateMain.Update(const SecondsPassed: Single; var HandleInput: Boolean);
 var
+  BootDots: String;
+  BootSecs: Integer;
   CurrentTheta: Single;
 begin
   inherited;
   { This virtual method is executed every frame.}
   LabelFps.Caption := 'FPS: ' + Container.Fps.ToString;
 
+  if not (BootStrapTimer = 0) then
+    begin
+      BootSecs := BootCount - Trunc((CastleGetTickCount64 - BootStrapTimer) / 1000);
+      if BootSecs > 0 then
+        begin
+          BootDots := '';
+          while (BootSecs > 0) do
+            begin
+              BootDots := BootDots + '.';
+              Dec(BootSecs);
+            end;
+          LabelBootStrap.Caption := BootStrapLabel + BootDots;
+        end
+      else
+        BootStrap;
+    end;
+	
   if not (Scene = nil) and Scene.IsVisible then
     begin
-	  if not(LoadTimer = 0) then
-	    begin
-		  LabelFirstFrame.Caption := 'Load Time = ' + FormatFloat('###0.000', (CastleGetTickCount64 - LoadTimer) / 1000) + ' seconds';
-		  LoadTimer := 0;
-		end;
+      if not(LoadTimer = 0) then
+	begin
+	      LabelFirstFrame.Caption := 'Load Time = ' +
+                FormatFloat('###0.000', (CastleGetTickCount64 - LoadTimer) / 1000) +
+                ' seconds' + LineEnding + GLVersion.Renderer;
+	      LoadTimer := 0;
+	end;
       if RotateSphere then
         begin
-          LabelAction.Caption := 'Sphere of ' + IntToStr(SphereObjs) + ' objects rotating once every ' + FormatFloat('#0.00', SecsPerRot) + ' seconds';
+          LabelAction.Caption := 'Sphere of ' + IntToStr(SphereObjs) + ' objects rotating at ' + FormatFloat('#0.000', 60 / SecsPerRot) + ' RPM';
           // Set angle (theta) to revolve completely once every SecsPerRot
           CurrentTheta := (((CastleGetTickCount64 mod
                      (SecsPerRot * 1000)) /
@@ -242,69 +275,125 @@ begin
 
 end;
 
+procedure TStateMain.SaveCurrentTheta;
+var
+  CurrentTheta: Single;
+begin
+  if RotateSphere then
+    begin
+      // If the scene is about to resume store an offset in SavedTheta
+      CurrentTheta := ((CastleGetTickCount64 mod
+				       (SecsPerRot * 1000)) /
+				       (SecsPerRot * 1000)) * (Pi * 2);
+      SavedTheta := SavedTheta - CurrentTheta;
+    end
+  else
+    begin
+      // Save the current rotation in SavedTheta
+      SavedTheta := Scene.Rotation.W; // W is the angle of a Vector4
+    end;
+end;
+
+function TStateMain.RebaseSavedTheta(const ADelta: Single): Single;
+var
+  NewTheta: Single;
+  OldTheta: Single;
+begin
+  OldTheta := ((CastleGetTickCount64 mod
+				   (SecsPerRot * 1000)) /
+				   (SecsPerRot * 1000)) * (Pi * 2);
+  NewTheta := ((CastleGetTickCount64 mod
+				   ((SecsPerRot + ADelta) * 1000)) /
+				   ((SecsPerRot + ADelta) * 1000)) * (Pi * 2);
+  
+  SavedTheta := SavedTheta + (OldTheta - NewTheta);
+  
+  Result := SecsPerRot + ADelta;
+
+end;
+
 function TStateMain.Press(const Event: TInputPressRelease): Boolean;
 begin
   if Event.IsKey(keyEscape) then // Why didn't I use a switch?
     begin
+      AbortEverything := True;
       Application.Terminate;
     end
   else if Event.IsKey(keyX) then
     begin
       LabelKeys.Exists := not LabelKeys.Exists;
+      LabelFps.Exists := not LabelFps.Exists;
+      LabelAction.Exists := not LabelAction.Exists;
+      LabelFirstFrame.Exists := not LabelFirstFrame.Exists;
     end
-  else if Event.IsKey(']') then
-    begin
-      if SecsPerRot > 0.25 then
-        begin
-          SaveTheta; // This don't work properly yet
-          SecsPerRot := SecsPerRot - 0.25;
-        end;
-    end
-  else if Event.IsKey('[') then
-    begin
-      SaveTheta; // This don't work properly yet
-      SecsPerRot := SecsPerRot + 0.25;
-    end
-  else if Event.IsKey(keyD) then
-    begin
-      SphereObjs := SphereObjs * 2;
-      // Show the load time, number of objects and scale
-      LoadScene(Models, SphereObjs);
-    end
-  // Double number of objects
-  else if Event.IsKey(keyH) then
-    begin
-      if SphereObjs >= MinSphereObjs then
-        begin
-          SphereObjs := SphereObjs div 2;
-          // Show the load time, number of objects and scale
-          LoadScene(Models, SphereObjs);
-        end;
-    end
-  // Toggle Camera Position (0, 0, 0) / normal
-  else if Event.IsKey(keyC) then
-    begin
-      // Switch CameraAtOrigin after saving settings!!!
-      CameraAtOrigin := not CameraAtOrigin;
-      SetCameraPosition;
-    end
-  else if Event.IsKey(keyR) then
-    begin
-      RotateSphere := Not RotateSphere;
-      SaveTheta;
-	end
   else if Event.IsKey(keyG) then
     begin
       Container.SaveScreen('grab.png');
-	end
+    end
   else if Event.IsKey(keySpace) then
     begin
-	  if not DoneBootstrap then
-		begin
-		  BootStrap;
-		end;
+      if not DoneBootstrap then
+        begin
+          BootStrap;
+        end;
+    end
+  else
+  // Only do anything with Scene if it's loaded
+  if not (Scene = nil) and Scene.IsVisible then
+    begin
+      if Event.IsKey(']') then
+        begin
+          if SecsPerRot > 0.25 then
+            begin
+              SecsPerRot := RebaseSavedTheta(-0.25);
+            end;
+        end
+      else if Event.IsKey('[') then
+        begin
+              SecsPerRot := RebaseSavedTheta(0.25);
+        end
+      // Halve number of objects
+      else if Event.IsKey(keyD) then
+        begin
+          SphereObjs := SphereObjs * 2;
+          LabelBootStrap.Exists := True;
+          {$ifdef userenderstack}
+          WaitForRenderAndCall(@LoadSceneOnUpdate);
+          {$else}
+          LoadScene(Models, SphereObjs);
+          {$endif}
+          LabelBootStrap.Exists := False;
+        end
+      // Double number of objects
+      else if Event.IsKey(keyH) then
+        begin
+          if SphereObjs >= MinSphereObjs then
+            begin
+              SphereObjs := SphereObjs div 2;
+              LabelBootStrap.Exists := True;
+              {$ifdef userenderstack}
+              WaitForRenderAndCall(@LoadSceneOnUpdate);
+              {$else}
+              LoadScene(Models, SphereObjs);
+              {$endif}
+              LabelBootStrap.Exists := False;
+            end;
+        end
+      // Toggle Camera Position (0, 0, 0) / normal
+      else if Event.IsKey(keyC) then
+        begin
+          // Switch CameraAtOrigin after saving settings!!!
+          CameraAtOrigin := not CameraAtOrigin;
+          SetCameraPosition;
+        end
+      // Toggle Rotation
+      else if Event.IsKey(keyR) then
+        begin
+          RotateSphere := Not RotateSphere;
+          SaveCurrentTheta;
+        end;
     end;
-
+  
   Result := inherited;
   if Result then Exit; // allow the ancestor to handle keys
 
@@ -326,29 +415,6 @@ begin
     Exit(true); // key was handled
   end;
   }
-end;
-
-procedure TStateMain.SaveTheta;
-var
-  CurrentTheta: Single;
-begin
-  // Only do anything with Scene if it's loaded
-  if not (Scene = nil) then
-    begin
-      if RotateSphere then
-        begin
-          // If the scene is about to resume store an offset in SavedTheta
-          CurrentTheta := ((CastleGetTickCount64 mod
-                           (SecsPerRot * 1000)) /
-                           (SecsPerRot * 1000)) * (Pi * 2);
-          SavedTheta := SavedTheta - CurrentTheta;
-        end
-      else
-        begin
-          // Save the current rotation in SavedTheta
-          SavedTheta := Scene.Rotation.W; // W is the angle of a Vector4
-        end;
-    end;
 end;
 
 procedure TStateMain.SetCameraPosition;
@@ -399,14 +465,21 @@ begin
 
   for Idx := 0 to ObjCount -1 do
     begin
+      if AbortEverything then
+        begin
+          Exit(nil);
+        end;
+
       // Adapted from the StackOverflow code
-      LabelAction.Caption := 'Building scene for ' + IntToStr(SphereObjs) + ' objects.' + LineEnding + 'Adding model ' + IntToStr(Idx + 1);
-	  Application.ProcessAllMessages;
+      LabelBootStrap.Caption := 'Building scene for ' + IntToStr(SphereObjs) + ' objects.' + LineEnding + 'Adding model ' + IntToStr(Idx + 1);
+      Application.ProcessAllMessages;
+
       YPos := 1 - (Idx / (ObjCount - 1)) * 2;
       Radius := sqrt(1 - (YPos * YPos));
       Theta := Phi * Idx;
       XPos := cos(Theta) * Radius;
       ZPos := sin(Theta) * Radius;
+
       // We not have X, Y and Z for the object so apply scale + translation
       TransformNode := TTransformNode.Create;
       TransformNode.Scale := Vector3(ObjScale, ObjScale, ObjScale);
@@ -422,7 +495,7 @@ begin
       GroupNode.AddChildren(TransformNode);
     end;
 
-  LabelAction.Caption := 'Scene constructed for ' + IntToStr(SphereObjs) + ' objects.' + LineEnding + 
+  LabelBootStrap.Caption := 'Scene constructed for ' + IntToStr(SphereObjs) + ' objects.' + LineEnding +
     'Elapsed time = ' + FormatFloat('###0.000', (CastleGetTickCount64 - LoadTimer) / 1000) + ' seconds' + LineEnding + 'CGE is doing other things ATM ... ';
   Application.ProcessAllMessages;
 
@@ -430,34 +503,37 @@ begin
   Result := GridNode;
 end;
 
+procedure TStateMain.LoadSceneOnUpdate(Sender: TObject);
+begin
+  LoadScene(Models, SphereObjs);
+end;
+
 procedure TStateMain.LoadScene(const SomeModels: TModelArray; const ObjectsOnSphere: Integer = 1);
 var
   SphereNode: TX3DRootNode;
 begin
-  if Assigned(Scene) then
-    begin
-      FreeAndNil(Scene);
-      LoadTimer := CastleGetTickCount64;
-    end;
-  // Create a scene
-  Scene := TCastleScene.Create(Self);
-
   {$ifdef usebatching}
   DynamicBatching := True;
   {$endif}
 
+  if Assigned(Scene) then
+    begin
+      FreeAndNil(Scene);
+    end;
+  LoadTimer := CastleGetTickCount64;
   // Load a model into the scene
   SphereNode := BuildSphere(Models, ObjectsOnSphere);
-  Scene.Load(SphereNode, True);
-//  Scene.Scale := Vector3(0.5, 0.5, 0.5);
-
-  // Add the scene to the viewport
-  Viewport.Items.Add(Scene);
-
-  // Tell the control this is the main scene so it gets some lighting
-  Viewport.Items.MainScene := Scene;
-  
-  SetCameraPosition;
+  if not(SphereNode = nil) then
+    begin
+      // Create a scene
+      Scene := TCastleScene.Create(Self);
+      Scene.Load(SphereNode, True);
+      // Add the scene to the viewport
+      Viewport.Items.Add(Scene);
+      // Tell the control this is the main scene so it gets some lighting
+      Viewport.Items.MainScene := Scene;
+      SetCameraPosition;
+    end;
 end;
 
 
